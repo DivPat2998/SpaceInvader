0258244ee9d8c48b3d78458654af67d418caf592
Divyangshu Patnaik <patnaik.divyangshu29@gmail.com>
2019-06-04 14:04:55 +0700

    SpaceInvaderNewImprovements2

--- app/src/main/java/com/example/newgame/GameActivity.java
+++ app/src/main/java/com/example/newgame/GameActivity.java
@@ -5,10 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
+
+    private GameView gameView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GameView game = new GameView(this);
+        gameView = new GameView(this);
         setContentView(game);
 
     }
@@ -16,6 +20,18 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        gameView.pause();
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/newgame/GameView.java
+++ app/src/main/java/com/example/newgame/GameView.java
@@ -5,11 +5,49 @@ import android.view.Surface;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable {
+    volatile boolean playing;
+
+    private Thread gameThread = null;
 
     public GameView(Context context) {
         super(context);
     }
 
     @Override
-    public void run() { }
+    public void run() {
+        while(playing){
+            update();
+
+            draw();
+
+            control();
+        }
+    }
+
+    private void update() {
+    }
+    private void draw(){
+
+    }
+    private void control(){
+        try{
+            gameThread.sleep(17);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public void  pause(){
+        playing = false;
+        try{
+            gameThread.join();
+
+        } catch (InterruptedException e){
+
+        }
+    }
+    public void resume(){
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 }
