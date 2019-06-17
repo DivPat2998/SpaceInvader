ad04ac6abf712b52a2176ff53ea66ad80d0afb75
Divyangshu Patnaik <patnaik.divyangshu29@gmail.com>
2019-06-17 14:53:37 +0700

    SpaceInvaderNewImprovements player control 2.0 and star background addition

--- app/src/main/java/com/example/newgame/GameView.java
+++ app/src/main/java/com/example/newgame/GameView.java
@@ -9,6 +9,8 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 public class GameView extends SurfaceView implements Runnable {
     volatile boolean playing;
 
@@ -22,6 +24,10 @@ public class GameView extends SurfaceView implements Runnable {
 
     private SurfaceHolder surfaceHolder;
 
+    private ArrayList<Star> stars = new
+
+            ArrayList<Star>();
+
     public GameView(Context context, int screenX, int screenY) {
         super(context);
 
@@ -29,6 +35,13 @@ public class GameView extends SurfaceView implements Runnable {
 
         surfaceHolder = getHolder();
         paint = new Paint();
+
+        int starNums = 100;
+        for(int i = 0; i < starNums; i++){
+            Star s = new Star(screenX, screenY);
+            stars.add(s);
+
+        }
 
     }
 
@@ -43,14 +56,25 @@ public class GameView extends SurfaceView implements Runnable {
 
     private void update() {
         player.update();
+        for(Star s : stars){
+            s.update(player.getSpeed());
+        }
     }
 
     private void draw(){
         if(surfaceHolder.getSurface().isValid()){
             canvas = surfaceHolder.lockCanvas();
             canvas.drawColor(Color.BLACK);
-            canvas.drawBitmap(player.getBitmap(),player.getX(),player.getY(),paint);
+            paint.setColor(Color.WHITE);
+
+            for(Star s : stars){
+                paint.setStrokeWidth(s.getStarWidth());
+                canvas.drawPoint(s.getX(), s.getY(),paint);
+            }
+            canvas.drawBitmap(player.getBitmap(),player.getX(), player.getY(), paint);
             surfaceHolder.unlockCanvasAndPost(canvas);
+
+
         }
 
     }
@@ -80,9 +104,12 @@ public class GameView extends SurfaceView implements Runnable {
     public boolean onTouchEvent(MotionEvent motionEvent){
         switch (motionEvent.getAction() & MotionEvent.ACTION_MASK){
             case MotionEvent.ACTION_UP:
+                player.stopBoosting();
                 break;
 
             case MotionEvent.ACTION_DOWN:
+
+                player.setBoosting();
 
                 break;
         }
