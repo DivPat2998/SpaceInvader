0e08ef3c84e5db8293f235a7f9704f7ec30a48f6
Adolf Dsilva <adolf@lyhourpay.com>
2019-06-30 13:19:06 +0700

    Added detectCollision Object.

--- app/src/main/java/com/example/newgame/Friend.java
+++ app/src/main/java/com/example/newgame/Friend.java
@@ -7,11 +7,15 @@ import android.graphics.Rect;
 
 import java.util.Random;
 
+/**
+ * Created by Manish on 10/24/2016.
+ */
+
 public class Friend {
 
     private Bitmap bitmap;
     private int x;
-    private  int y;
+    private int y;
     private int speed = 1;
 
     private int maxX;
@@ -20,7 +24,11 @@ public class Friend {
     private int maxY;
     private int minY;
 
-    public Friend(Context context, int screenX, int screenY){
+    //creating a rect object for a friendly ship
+    private Rect detectCollision;
+
+
+    public Friend(Context context, int screenX, int screenY) {
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.friend);
         maxX = screenX;
         maxY = screenY;
@@ -29,28 +37,47 @@ public class Friend {
         Random generator = new Random();
         speed = generator.nextInt(6) + 10;
         x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
 
+        //initializing rect object
+        detectCollision = new Rect(x, y, bitmap.getWidth(), bitmap.getHeight());
     }
-        public void update(int playerSpeed){
+
+    public void update(int playerSpeed) {
         x -= playerSpeed;
         x -= speed;
-        if(x < minX - bitmap.getWidth()){
+        if (x < minX - bitmap.getWidth()) {
             Random generator = new Random();
-            speed = generator.nextInt(10)+10;
+            speed = generator.nextInt(10) + 10;
             x = maxX;
             y = generator.nextInt(maxY) - bitmap.getHeight();
-
-            }
-
         }
 
-    public Bitmap getBitmap(){
+        //Adding the top, left, bottom and right to the rect object
+        detectCollision.left = x;
+        detectCollision.top = y;
+        detectCollision.right = x + bitmap.getWidth();
+        detectCollision.bottom = y + bitmap.getHeight();
+    }
+
+
+
+    //one more getter for getting the rect object
+    public Rect getDetectCollision() {
+        return detectCollision;
+    }
+
+    //getters
+    public Bitmap getBitmap() {
         return bitmap;
     }
-    public int getX(){
+
+    public int getX() {
         return x;
     }
-    public int getY(){
+
+    public int getY() {
         return y;
     }
+
 }
